e36892119bdac97e39eee200c3ea438489e4193d
Aayush <aayushc951@gmail.com>
2026-08-12 07:14:32 +0530

    added: spring security frontend

--- SpringSecurityDemoProject/src/main/java/com/ibm/ecms/repository/CustomerRespository.java
+++
@@ -1,8 +0,0 @@
-package com.ibm.ecms.repository;
-
-import com.ibm.ecms.entity.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRespository extends CrudRepository<Customer, Integer> {
-
-}
